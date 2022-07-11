869fabbeae8a349a1db8a0ffd31a6fa2cf64e9cf
Davran Dilshat <davran.dilshat@gmail.com>
2022-07-11 20:49:30 +0100

    start of new logic

--- Game.cs
+++ Game.cs
@@ -5,6 +5,8 @@ namespace LunarLander
 {
     public class Game : Node
     {
+        private const float TOTAL_MOVING_TIME = 0.8f;
+
         private readonly GameLogic Logic = new GameLogic();
 
         private ParallaxBackground StarsParallax;
@@ -17,8 +19,25 @@ namespace LunarLander
 
         private Label AltitudeLabel;
 
+        private bool isUserHoldingDown;
+
+        private float pendingFuelToBurn;
+
+        private float movingTime;
+
+        private GameState State = GameState.Pending;
+
+        private enum GameState
+        {
+            Pending,
+            Moving,
+            Over
+        }
+
         public override void _Ready()
         {
+            SetPhysicsProcess(false);
+
             StarsParallax = GetNode<ParallaxBackground>("StarsParallax");
             Background = GetNode<Sprite>("BackgroundLayer/Background");
             RocketAnimationPlayer = GetNode<AnimationPlayer>("Rocket/AnimationPlayer");
@@ -26,31 +45,67 @@ namespace LunarLander
             AltitudeLabel = GetNode<Label>("HUD/Altitude/Label");
 
             AltitudeProgressBar.MaxValue = 1000;
+            StarsParallax.ScrollOffset = new Vector2(0, Logic.Altitude);
+        }
+
+        public override void _Process(float delta)
+        {
+            UpdateHud();
+
+            // TODO: only anim?
+            if (Logic.Altitude <= 0)
+                EndGame();
+
+            if (isUserHoldingDown && pendingFuelToBurn < Logic.FuelRemaining)
+            {
+                // TODO: exponential
+                pendingFuelToBurn += delta * 300;
+                RocketAnimationPlayer.Play("holding");
+            }
+            else if (pendingFuelToBurn > 0)
+            {
+                Logic.BurnFuel(pendingFuelToBurn);
+                pendingFuelToBurn = 0;
+                RocketAnimationPlayer.Play("boost");
+
+                movingTime = 0;
+                SetPhysicsProcess(true);
+                SetProcess(false);
+            }
         }
 
         public override void _PhysicsProcess(float delta)
         {
-            if (Logic.Altitude <= 0)
-                EndGame();
-
             UpdateHud();
+
+            if (movingTime > TOTAL_MOVING_TIME)
+            {
+                StarsParallax.ScrollOffset = new Vector2(0, Logic.Altitude);
+                SetPhysicsProcess(false);
+                SetProcess(true);
+                return;
+            }
+
+            movingTime += delta;
+            StarsParallax.ScrollOffset -= new Vector2(0, Logic.Velocity * delta / TOTAL_MOVING_TIME);
         }
 
         public override void _Input(InputEvent inputEvent)
         {
             if (inputEvent.IsActionPressed("restart"))
                 GetTree().ReloadCurrentScene();
+
+            if (inputEvent is InputEventScreenTouch touchEvent)
+                isUserHoldingDown = touchEvent.Pressed;
         }
 
         private void UpdateHud()
         {
-            // TODO: background offset
-            StarsParallax.ScrollOffset = new Vector2(0, Logic.Altitude);
             AltitudeProgressBar.Value = Logic.Altitude;
             AltitudeLabel.Text = $"{(int)Logic.Altitude}m";
 
             // TODO: should be removed or hidden
-            GetNode<Label>("Info").Text = $"Velocity:{Logic.Velocity}\nFuelRemaining:{Logic.FuelRemaining}";
+            GetNode<Label>("Info").Text = $"Velocity:{Logic.Velocity}\nFuelRemaining:{Logic.FuelRemaining}\nPendingFuelToBurn:{pendingFuelToBurn}\nStarsParallax.ScrollOffset:{StarsParallax.ScrollOffset}";
         }
 
         private void EndGame()
--- GameLogic.cs
+++ GameLogic.cs
@@ -4,7 +4,7 @@ namespace LunarLander
 {
     public class GameLogic
     {
-        private const float FUEL_POWER = 0.15f;
+        private const float FUEL_POWER = 0.12f;
 
         public float Velocity { get; private set; } = 0;
 
@@ -13,7 +13,7 @@ namespace LunarLander
         public float FuelRemaining { get; private set; } = 1000;
 
         /// <Summary>Continues the game by one in-game second.</Summary>
-        public void Next(float fuelToBurn)
+        public void BurnFuel(float fuelToBurn)
         {
             if (fuelToBurn > FuelRemaining)
                 fuelToBurn = FuelRemaining;
