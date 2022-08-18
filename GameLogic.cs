e49587381e5555bf2e8af9f07337b6c091fe9fb4
Davran Dilshat <davran.dilshat@gmail.com>
2022-08-18 12:39:43 +0100

    add logic history

--- GameLogic.cs
+++ GameLogic.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace LunarLander
 {
@@ -6,13 +7,15 @@ namespace LunarLander
     {
         private const float FUEL_POWER = 0.15f;
 
-        public int SecondsPassed { get; private set; }
-
         public float Velocity { get; private set; } = 0;
 
         public float Altitude { get; private set; } = 1000;
 
         public int FuelRemaining { get; private set; } = 1000;
+
+        public int[] History => history.ToArray();
+
+        private readonly List<int> history = new List<int>();
 
         /// <Summary>Continues the game by one in-game second.</Summary>
         public void Burn(int fuelToBurn)
@@ -24,7 +27,7 @@ namespace LunarLander
             Velocity += 1.6f - (fuelToBurn * FUEL_POWER);
             Altitude = Math.Max(0, Altitude - Velocity);
 
-            SecondsPassed++;
+            history.Add(fuelToBurn);
         }
     }
 }
